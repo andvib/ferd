61a8d53c85a70fcedffe95fdbd0111d2cb2b640b
andvib <andvibeto@gmail.com>
2023-03-15 21:26:59 +0100

    libs: Add rectangle class
    Add module for drawing rectangles with color.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "framework/gameShader.hpp"
 #include "framework/gameProgram.hpp"
 #include "gameTriangle.hpp"
+#include "train/rectangle.hpp"
 
 int main(void)
 {
@@ -56,6 +57,22 @@ int main(void)
     struct triangle_color c2 = {0, 0, 1};
     GameTriangle triangle2(t2, c2);
 
+    struct rectangle_points r1 = {-0.75, -0.75, -0.75, 0, 0, 0, 0, -0.75};
+    struct rectangle_color rc1 = {0, 0.25, 1};
+    Rectangle rectangle1(r1, rc1);
+
+    struct rectangle_points r2 = {0.75, 0.75, 0.75, 0, 0, 0, 0, 0.75};
+    struct rectangle_color rc2 = {0, 0.75, 1};
+    Rectangle rectangle2(r2, rc2);
+
+    struct rectangle_points r3 = {0, 0, 0, 0.75, -0.75, 0.75, -0.75, 0};
+    struct rectangle_color rc3 = {0, 0.75, 0.5};
+    Rectangle rectangle3(r3, rc3);
+
+    struct rectangle_points r4 = {0, 0, 0, -0.75, 0.75, -0.75, 0.75, 0};
+    struct rectangle_color rc4 = {0, 0.8, 0.75};
+    Rectangle rectangle4(r4, rc4);
+
     /* Loop until the user closes the window */
     while (window.isWindowRunning())
     {
@@ -65,8 +82,12 @@ int main(void)
 
         glUseProgram(program.getProgramID());
 
-        triangle1.drawObject();
-        triangle2.drawObject();
+        //triangle1.drawObject();
+        //triangle2.drawObject();
+        rectangle1.drawObject();
+        rectangle2.drawObject();
+        rectangle3.drawObject();
+        rectangle4.drawObject();
 
 		/* Swap front and back buffers */
 		err = window.swapBuffers();
