733a54cd190d73f1341f42b70816c1ea6ae0907e
andvib <andvibeto@gmail.com>
2023-07-02 10:25:23 +0200

    lib: train: Give trains a maximum speed

--- lib/game/trainPhysics.cpp
+++ lib/game/trainPhysics.cpp
@@ -7,7 +7,9 @@
 TrainPhysics::TrainPhysics()
 {
     m_speed = {0, 0};
+    m_max_speed = 0.002;
     m_acceleration = 0.00001;
+    m_acc_direction = 1;
     m_pos = {0, 4};
     m_vector = {-1, 0};
 }
@@ -15,20 +17,28 @@ TrainPhysics::TrainPhysics()
 TrainPhysics::TrainPhysics(position_t start_position, float acceleration)
 {
     m_speed = {0, 0};
+    m_max_speed = 0.002;
     m_acceleration = acceleration;
+    m_acc_direction = 1;
     m_pos = start_position;
     m_vector = {-1, 0};
 }
 
 void TrainPhysics::Update(clock_t delta_time_ms)
 {
-    m_speed.x = (m_acceleration * m_vector.x * delta_time_ms) + m_speed.x;
-    m_speed.y = (m_acceleration * m_vector.y * delta_time_ms) + m_speed.y;
+    float curr_abs_speed = sqrt(pow(m_speed.x, 2) + pow(m_speed.y, 2));
+
+    if (curr_abs_speed > m_max_speed) {
+        m_acc_direction = 0;
+    }
+
+    m_speed.x = (m_acceleration * m_acc_direction * m_vector.x * delta_time_ms) + m_speed.x;
+    m_speed.y = (m_acceleration * m_acc_direction * m_vector.y * delta_time_ms) + m_speed.y;
 
     m_pos.x = m_pos.x + (m_speed.x * delta_time_ms)
-              - (0.5 * m_acceleration * m_vector.x * delta_time_ms * delta_time_ms);
+              - (0.5 * m_acceleration * m_acc_direction * m_vector.x * delta_time_ms * delta_time_ms);
     m_pos.y = m_pos.y + (m_speed.y * delta_time_ms)
-              - (0.5 * m_acceleration * m_vector.y * delta_time_ms * delta_time_ms);
+              - (0.5 * m_acceleration * m_acc_direction * m_vector.y * delta_time_ms * delta_time_ms);
 }
 
 void TrainPhysics::stop() {
