19637b0b84d714a14930cf86976b0c0aeca6e08f
andvib <andvibeto@gmail.com>
2023-09-06 21:52:29 +0200

    lib: line: Fix broken for-loop

--- lib/game/line.cpp
+++ lib/game/line.cpp
@@ -4,13 +4,8 @@ Line::Line(std::vector<LineWaypoint *> waypoints) { v_waypoints = waypoints; }
 
 position_t Line::GetNextStation(int index, LineDirection direction) {
   if (direction == LineDirection::FORWARD) {
-    for (int i = index + 1; i < v_waypoints.size(); ++i) {
-      return v_waypoints[i]->PositionCoordinates();
-    }
+    return v_waypoints[index + 1]->PositionCoordinates();
   } else {
-    for (int i = index - 1; i >= 0; --i) {
-      return v_waypoints[i]->PositionCoordinates();
-    }
+    return v_waypoints[index - 1]->PositionCoordinates();
   }
-  return v_waypoints[0]->PositionCoordinates();
 }
