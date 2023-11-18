1c01a95d589adae1ddb77a6c41764c80b11723a6
andvib <andvibeto@gmail.com>
2023-11-18 14:45:26 +0100

    framework: Add Zoom to game camera

--- lib/game/camera.cpp
+++ lib/game/camera.cpp
@@ -2,20 +2,14 @@
 
 #include "spdlog/spdlog.h"
 
-Camera::Camera() {
-  Position = {0, 0};
-  Dimensions = {-5.0f, 5.0f, -5.0f, 5.0f};
-  Speed = 0.1;
-}
-
 glm::mat4 Camera::calculateViewProjMatrix() {
   glm::mat4 View = glm::lookAt(
       glm::vec3(Position.x, Position.y, 5),  // Position of camera
       glm::vec3(Position.x, Position.y, 0),  // Point camera looks at
       glm::vec3(0, 1, 0));                   // Up vector
-  glm::mat4 Projection =
-      glm::ortho(Dimensions.left, Dimensions.right, Dimensions.bottom,
-                 Dimensions.top, 0.0f, 100.0f);
+  glm::mat4 Projection = glm::ortho(
+      Dimensions.left * ZoomLevel, Dimensions.right * ZoomLevel,
+      Dimensions.bottom * ZoomLevel, Dimensions.top * ZoomLevel, 0.0f, 100.0f);
 
   return Projection * View;
 }
